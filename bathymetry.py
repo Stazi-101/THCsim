d6878ce0368efb0556dd7e7f4ef8ccecddcbdc2b
Stazi-101 <39802426+Stazi-101@users.noreply.github.com>
2024-03-08 11:59:11 +0000

    add some bathymetry ref files

--- bathymetry.py
+++ bathymetry.py
@@ -15,12 +15,13 @@ def get_map_path(config):
 
 def load_np(config):
 
-    image_np = cv2.imread(get_map_path(config), cv2.IMREAD_GRAYSCALE) 
+    image_np = cv2.imread(get_map_path(config)) 
+    
 
     #image_jax = jnp.array(image_np)
     
     #return image_jax
-    return image_np
+    return image_np[:,:,::-1]
 
 
 
@@ -28,15 +29,19 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
     
     config = {"map": {"mask_image_path_relative": True, 
-                      "mask_image_path": "resources\map\equirectangular_1920_wikipedia_blue_marble_2002.png"}}
+                      "mask_image_path": "resources\map\gebco_08_rev_bath_3600x1800_color.jpg"}}
     
-    image_np = load_np(config)
-    print(image_np.shape)
-    print(np.unique(image_np))
+    im = load_np(config).astype(int)
+    print(im.shape)
+    #image_np = cv2.resize(image_np, dsize=(90,45), interpolation=cv2.INTER_NEAREST_EXACT)
+    #print(image_np.shape)
+    im[:,:,:] += (np.isclose(im[:,:,0],im[:,:,2], atol=4))[:,:,np.newaxis]*255
+    #print(np.unique(image_np))
 
     #image_np = image_jax.numpy()
+    im[im>255]=255
 
-    plt.imshow(image_np)
+    plt.imshow(im)
     plt.show()
 
 
